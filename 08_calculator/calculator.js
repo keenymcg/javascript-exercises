74867f611042621e774a12bffd4bbd5a4072aa8e
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2023-03-14 21:39:52 -0700

    Attempt 2 at Calc funcs

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(input1, input2) {
+	return input1 + input2;
 };
 
-const subtract = function() {
-	
+const subtract = function(input1, input2) {
+	return input1 - input2;
 };
 
-const sum = function() {
-	
+const sum = function(input1) {
+	return input1.reduce((total, current) => {
+    return total + current;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(input1) {
+  let multTotal = 1
+  for (let x in input1) {
+    multTotal *= input1[x];
+  }
+  return multTotal;
 };
 
-const power = function() {
-	
+const power = function(input1, input2) {
+	return input1 ** input2;
 };
 
-const factorial = function() {
-	
+const factorial = function(input1) {
+  if (input1 === 0 || input1 === 1) {
+    return 1
+  } else {
+    for (let i = input1 - 1; i >= 1; i--) {
+      input1 *= i;
+    }
+  }
+  return input1;
 };
 
 // Do not edit below this line
