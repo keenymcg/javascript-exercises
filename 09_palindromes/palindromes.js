6af8e31437b4fdcf9b6cd6592ac3327b0074b134
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2023-03-17 12:33:51 -0700

    Account for spaces and commas

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,34 +1,44 @@
+// TO DO: update and store original palindrome parameter in a new variable that maybe uppers it, but definitely excludes the spaces
+
 const palindromes = function (palindrome) {
+    // I think I want to store the parameter in a new variable where all the spaces have been removed
+    let updateOrigPali = palindrome.replace(/\s/g, "");
+    // console.log(updateOrigPali);
+
     let currentChar = "";
     let letterArray = [];
     let puncArray = [];
     let alphaCharUpper = ""
 
-    for (let x in palindrome) {
-        currentChar = palindrome.charAt(x);
+    for (let x in updateOrigPali) {
+        currentChar = updateOrigPali.charAt(x);
 
-        let pattern = /[a-zA-Z]/; // matches any letter in the English alphabet (uppercase or lowercase)
+        let pattern = /[a-zA-Z\s,]/; // matches any letter in the English alphabet (uppercase or lowercase)
         if (pattern.test(currentChar)) {
             // console.log("The letter is in the English alphabet.");
             alphaCharUpper = currentChar.toUpperCase();
             letterArray.unshift(alphaCharUpper);
         } else {
             // console.log("The letter is not in the English alphabet.");
+            // console.log(currentChar);
             puncArray.unshift(currentChar);
         }
     }
     let mergedArray = letterArray.concat(puncArray);
+    console.log(mergedArray);
     let mergedString = mergedArray.join('');
-    if (mergedString === palindrome.toUpperCase()) {
-        // console.log(true);
-        return true;
+    console.log(mergedString);
+
+    if (mergedString === updateOrigPali.toUpperCase()) {
+        console.log(true);
+        // return true;
     } else {
-        //console.log(false);
-        return false;
+        console.log(false);
+        // return false;
     }
 };
 
-palindromes("OGGo!");
+palindromes("tac o, o cat!");
 
 // try running the test again but with console.logs in the spec file to see the difference between the given and expected output
 // todo: how to account for spaces between words? and commas?
