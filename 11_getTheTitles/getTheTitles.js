8c54aa39aa7d5c6a9e3b73c8f0af6814ac2ae27d
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2023-03-20 17:55:46 -0700

    Finish task

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
 const getTheTitles = function() {
-
+    let result = books.map(a => a.title);
+    return result
 };
+
+const books = [
+    {
+        title: 'Book',
+        author: 'Name'
+        },
+        {
+        title: 'Book2',
+        author: 'Name2'
+    }
+]
+
+// getTheTitles(books);
+// console.log(books.title);
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
