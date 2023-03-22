c8fd7a9fe6fed258a9039e6e41f8be3d54c8005c
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2023-03-21 17:40:12 -0700

    Clean final solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -2,10 +2,8 @@ function findTheOldest(people) {
     let nameOfOldest = ""
     let oldest = 0
     const stillAlive = new Date().getFullYear();
-    //console.log(people.length);
 
     for (let i = 0; i < people.length; i++) {
-        //console.log(people).name;
         if (!people[i].yearOfDeath) {
             people[i].yearOfDeath = stillAlive
         }
