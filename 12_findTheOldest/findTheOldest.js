7228ac8974c96232b4b5434490ace4cb13ebbac8
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2023-03-21 17:39:37 -0700

    Pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+function findTheOldest(people) {
+    let nameOfOldest = ""
+    let oldest = 0
+    const stillAlive = new Date().getFullYear();
+    //console.log(people.length);
 
+    for (let i = 0; i < people.length; i++) {
+        //console.log(people).name;
+        if (!people[i].yearOfDeath) {
+            people[i].yearOfDeath = stillAlive
+        }
+
+        let yearsAlive = (people[i].yearOfDeath - people[i].yearOfBirth);
+        console.log(yearsAlive);
+
+        if (yearsAlive > oldest) {
+            oldest = yearsAlive;
+            nameOfOldest = people[i].name
+        };
+    };
+    return nameOfOldest;
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
